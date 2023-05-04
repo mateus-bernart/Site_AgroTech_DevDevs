bc4a4dd62291fbfbca08cd8660d649dfad333cd4
Mateus Bernart <80543817+mateus-bernart@users.noreply.github.com>
2023-05-04 00:48:22 -0300

    Reveal transition & add infos

---
+++ main.js
@@ -0,0 +1,36 @@
+
+/* Event listner par aver se o DOM carregou antes do JavaScript
+ - Um dos problemas enfrentados.
+*/
+
+document.addEventListener("DOMContentLoaded", function() {
+    // your JavaScript code here
+    const btns = document.querySelectorAll(".nav-btn");
+    const slides = document.querySelectorAll(".video-slide");
+    const contents = document.querySelectorAll(".content");
+
+var sliderNav = function(manual){
+    btns.forEach((btn) => {
+        btn.classList.remove("active");
+    });
+    
+    slides.forEach((slide) => {
+        slide.classList.remove("active");
+    });
+    
+    contents.forEach((content) => {
+        content.classList.remove("active");
+    });
+
+    btns[manual].classList.add("active");
+    slides[manual].classList.add("active");
+    contents[manual].classList.add("active");
+
+}
+
+btns.forEach((btn, i) => {
+    btn.addEventListener("click", () => {
+        sliderNav(i);
+    });
+});
+  });
